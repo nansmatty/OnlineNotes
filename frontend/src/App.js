9af73d3e78d4489ab8abbf9ee1206aae23a707ef
Narayan Maity <narayanmaitysp1997@gmail.com>
2021-01-12 19:59:30 +0530

    Second commmit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import { Container } from 'react-bootstrap';
 import Footer from './components/Footer';
@@ -7,17 +7,25 @@ import MainScreen from './screen/MainScreen';
 import LoginScreen from './screen/LoginScreen';
 import NotesScreen from './screen/NotesScreen';
 import RegisterScreen from './screen/RegisterScreen';
+import { useDispatch, useSelector } from 'react-redux';
 
 const App = () => {
+  const userLogin = useSelector((state) => state.userLogin);
+  const { userInfo } = userLogin;
+
   return (
     <Router>
       <Header />
       <main className='py-3'>
         <Container>
-          <Route path='/' component={MainScreen} exact />
-          <Route path='/login' component={LoginScreen} exact />
-          <Route path='/register' component={RegisterScreen} exact />
-          <Route path='/notes' component={NotesScreen} exact />
+          {!userInfo && (
+            <Fragment>
+              <Route path='/' component={MainScreen} exact />
+              <Route path='/login' component={LoginScreen} exact />
+              <Route path='/register' component={RegisterScreen} exact />
+            </Fragment>
+          )}
+          {userInfo && <Route path='/notes' component={NotesScreen} exact />}
         </Container>
       </main>
       <Footer />
